37fbd5413799b671fd9e855263a3e58974310b6b
LizaChepurko <139445067+LizaChepurko@users.noreply.github.com>
2023-07-13 22:18:52 +0300

    Update MainOfBT.java

--- MainOfBT.java
+++ MainOfBT.java
@@ -7,82 +7,12 @@ import java.util.ArrayList;
 public class MainOfBT {
     public static <T> void main(String[] args) throws Exception {
 
-        BinaryTree1<Integer> bt1 = new BinaryTree1<Integer>();
-        bt1.add(1);
-        bt1.add(2);
-        bt1.add(3);
-        bt1.add(4);
-        bt1.add(5);
-        bt1.add(6);
-        bt1.add(7);
-        bt1.add(8);
-        bt1.add(9);
-        TreePrinter(bt1);
-        System.out.println("-------");
-        System.out.println(ArrayofEvenLeaves(bt1));
 
     }
-    public static void printArr(int[] arr) {
-        if (arr != null) {
-            System.out.print("[");
-            for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + ",");
-            }
-            System.out.print("]");
-        }
-
-    }
-
-    //Exam B
-    public static int minLevel(BinaryTree bt) {
-        if(bt == null){return -1;}
-        int l = minLevel(bt.getLeft());
-        int r = minLevel(bt.getRight());
-        return 1+ Math.min(l,r);
-
-    }
-    // set == add
-    public static <T> void addClosest(BinaryTree<T> bt, T c){
-        addClosestH(bt,c,minLevel(bt));
-    }
-
-    private static <T> void addClosestH(BinaryTree<T> bt, T c, int j) {
-        if(j == 0){
-            bt.add(c);
-        }
-        else{
-            if(minLevel(bt.getLeft()) <= minLevel(bt.getRight())){
-                addClosestH(bt.getLeft(), c , j-1);
-            }
-            else{
-                addClosestH(bt.getRight(), c , j-1);
-            }
-        }
-    }
-    public static int minLevelLeaf(BinaryTree bt) {
-        int ans = -1;
-        if (bt != null) {
-            if (bt.size() == 1) {
-                ans = 0;
-            } else {
-                if (bt.getLeft() == null && bt.getRight() != null) {
-                    return 1+ minLevelLeaf(bt.getRight());
-                }
-                if (bt.getLeft() != null && bt.getRight() == null) {
-                    return 1+ minLevelLeaf(bt.getLeft());
-                }
-                if (bt.getLeft() != null && bt.getRight() != null) {
-                    return 1 + Math.min(minLevelLeaf(bt.getLeft()),minLevelLeaf(bt.getRight()));
-                }
-            }
-        }
-        return ans;
-    }
-    public static <T> int count(BinaryTree bt, T c){
-        if(bt == null){return 0;}
-        if(bt.getRoot().equals(c)){return 1;}
-        return 1+ count(bt.getLeft(), c) + count(bt.getRight(),c);
-    }
+    
+    /**
+     * This function recursively traverses the binary tree and counts the nodes that have a left child but no right child.
+     */
     public static <T> int LeftSon(BinaryTree bt){
         if(bt == null ||bt.size() == 1){return 0;}
         if(bt.getLeft() != null && bt.getRight() == null ){return 1;}
@@ -90,6 +20,13 @@ public class MainOfBT {
             return LeftSon(bt.getLeft()) + LeftSon(bt.getRight());
         }
     }
+    
+    /**
+     * This function counts the number of leaves in a binary tree at even levels.
+     * It uses a helper function, EvenLevelsH, to recursively traverse the binary tree and determine
+     * the number of leaves at even levels.
+     * NOTE: the lower level is zero.
+     */
     public static <T> int EvenLevels(BinaryTree bt){
         return EvenLevelsH(bt,0);
     }
@@ -102,8 +39,11 @@ public class MainOfBT {
             int r = EvenLevelsH(bt.getRight(), n+1);
             return l+r;
             }
-
     }
+    /**
+     *This function returns an ArrayList of all the leaves in even levels. 
+     * NOTE: the lower level is zero.
+     */
     public static <T> ArrayList<T> ArrayofEvenLeaves(BinaryTree<T> bt){
         ArrayList<T> ans = new ArrayList<>();
         return ArrayofEvenLeavesH(bt,ans,0);
@@ -122,6 +62,9 @@ public class MainOfBT {
         return ans;
     }
 
+    /**
+     *This function returns an ArrayList of all the leaves in a specific order. 
+     */
 
     public static <T> ArrayList<T> sortedArray(BinaryTree<T> bt, int s) throws Exception {
         ArrayList<T> a = new ArrayList<>();
@@ -138,7 +81,6 @@ public class MainOfBT {
             throw new Exception("You should enter number between 1-3");
         }
     }
-
 
     private static <T> ArrayList<T> InorderSort(BinaryTree<T> bt, ArrayList<T> ans) {
         if(bt == null){return ans;}
@@ -170,6 +112,13 @@ public class MainOfBT {
         }
         return ans;
     }
+    
+    /**
+     * bt: The binary tree to be traversed.
+     * Inorder, PreOrder, and PostOrder, for traversing a binary tree.
+     * Each method performs a different type of traversal and helps you
+     * visit the nodes of the tree in a specific order.
+     */
 
     public static <T> void Inorder(BinaryTree<T> bt){
        if(bt == null){return;}
@@ -198,49 +147,9 @@ public class MainOfBT {
         }
     }
 
-    public static int[] numberOfLeafsByLevel(BinaryTree bt) {
-        int[] ans = null;
-        if (bt != null && bt.size() > 0) {
-            ans = new int[height(bt)];
-            numberOfLeafsByLevelH(bt, ans, 0);
-        }
-        return ans;
-    }
-        private static void numberOfLeafsByLevelH(BinaryTree bt,int[] ans, int d){
-            if (bt != null && bt.size() > 0) {
-                if (bt.size() == 1) {
-                    ans[d]++;
-                } else {
-                    numberOfLeafsByLevelH(bt.getLeft(), ans, d + 1);
-                    numberOfLeafsByLevelH(bt.getRight(), ans, d + 1);
-                }
-            }
-        }
-
-        public static<T> void TreePrinter(BinaryTree<T> tree) {
-        int h = height(tree);
-        int col = getcol(h);
-        int[][] M = new int[h][col];
-        printTree(M, tree, col / 2, 0, h);
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < col; j++) {
-                if (M[i][j] == 0)
-                    System.out.print("  ");
-                else
-                    System.out.print(M[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static int getHeight(BinaryTree bt){
-        if (bt == null) {
-            return -1;
-        }
-        return 1+Math.max(getHeight(bt.getLeft()),getHeight(bt.getRight()));
-
-    }
-
+   /**
+     *This function returns an ArrayList of all the leaves with no specific order. 
+     */
     public static ArrayList<Integer> arrayListOfLeaves(BinaryTree<Integer> bt){
         ArrayList<Integer> ans = new ArrayList<>();
         if(bt != null && bt.size()>0) {
@@ -258,46 +167,6 @@ public class MainOfBT {
         }
         return ans;
 
-    }
-    //q8
-    public static boolean isOfTheSameStructure(BinaryTree bt1, BinaryTree bt2){
-        if(bt1 == null && bt2 == null){
-            return true;
-        }
-        else if(bt1!=null && bt2!=null){
-            return isOfTheSameStructure(bt1.getRight(),bt2.getRight())
-                    && isOfTheSameStructure(bt1.getLeft(),bt2.getLeft());
-        }
-        return false;
-    }
-    public static <T> int numOfleavs(BinaryTree<T> bt) {
-        if(bt == null) {return 0;}
-        if(bt.getLeft()==null && bt.getRight() == null) {return 1;}
-        return numOfleavs(bt.getLeft())+ numOfleavs(bt.getRight());
-    }
-
-
-
-
-
-    public static int getcol(int h) {
-        if (h == 1)
-            return 1;
-        return getcol(h - 1) + getcol(h - 1) + 1;
-    }
-
-    public static <T> void printTree(int[][] M, BinaryTree<T> root, int col, int row, int height) {
-        if (root == null)
-            return;
-        M[row][col] = (int)root.getRoot();
-        printTree(M, root.getLeft(), col - (int)Math.pow(2, height - 2), row + 1, height - 1);
-        printTree(M, root.getRight(), col + (int)Math.pow(2, height - 2), row + 1, height - 1);
-    }
-
-    public static<T> int height(BinaryTree<T> root) {
-        if (root == null)
-            return 0;
-        return 1+ Math.max(height(root.getLeft()), height(root.getRight()));
     }
 
 }
